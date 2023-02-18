21fdfdd6f260c879bb391ccd266b5ce22e2b2f15
Antonio José Masiá <email@antoniomasia.com>
2023-02-18 23:57:48 +0100

    add initial lsp config with lsp-zero plugin

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -1,3 +1,4 @@
 require("user.plugins.themes")
 require("user.plugins.treesitter")
 require("user.plugins.telescope")
+require("user.plugins.lsp")
---
+++ lua/user/plugins/lsp.lua
@@ -0,0 +1,19 @@
+local status_ok, lsp = pcall(require, "lsp-zero")
+if not status_ok then
+	return
+end
+
+print("LSP-ZERO")
+
+lsp.preset({
+	name = "recommended",
+	set_lsp_keymaps = true,
+	manage_nvim_cmp = true,
+	suggest_lsp_servers = false
+})
+
+-- When you don't have mason.nvim installed
+-- You'll need to list the servers installed in your system
+lsp.setup_servers({ "lua_ls", force = true })
+
+lsp.setup()
